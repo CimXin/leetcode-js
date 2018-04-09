ac6318b79c7b59047e987dd6a7ab6ff6d45cfa59
CimXin <372134690@qq.com>
2018-04-09 16:07:34 +0800

    add

---
+++ 7.Reverse Integer.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function (x) {
+    var symbol = x < 0 ? -1 : 1;
+    var xStr = x.toString();
+    var len = xStr.length;
+    var yStr = ""
+    for (var i = xStr.length - 1; i >= 0; --i) {
+        yStr += xStr[i];
+    }
+    x =  parseInt(yStr) * symbol;
+
+    if (x > Math.pow(2, 31) - 1 || x < -Math.pow(2, 31)) {
+        return 0;
+    }
+    return x;
+};
