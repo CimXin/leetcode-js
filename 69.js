ab1599ea57d34f4bf98c939add1a1956ad5b2ef9
CimXin <372134690@qq.com>
2018-07-23 09:30:22 +0800

    update 69

---
+++ 69.js
@@ -0,0 +1,12 @@
+//Q: https://leetcode-cn.com/problems/sqrtx/description/
+
+/**
+ * 这题-_-!!
+ */
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function (x) {
+    return Math.sqrt(x) << 0;
+};
